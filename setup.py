96ceb93f1730e163e9fedca1e5dec44463538e05
dskarls <dskpersistent@gmail.com>
2019-11-08 09:31:09 -0800

    VER: Bump to package 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kim-python-utils",
-    version="0.1.0",
+    version="0.2.0",
     description=(
         "Helper routines for querying the OpenKIM Repository and writing KIM Tests and Verification Checks"
     ),
